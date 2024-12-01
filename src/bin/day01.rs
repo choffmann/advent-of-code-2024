e7c9fbf4d3d0c1726d0e7c8c83da7fde3c5f0f59
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-12-01 16:14:44 +0100

    feat: solve day01 part 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,4 +1,4 @@
-use std::u32;
+use std::collections::HashMap;
 
 use aoc_2024::Problem;
 
@@ -25,7 +25,28 @@ impl Problem for Day01 {
     }
 
     fn part_two(&self, input: &str) -> String {
-        todo!()
+        let (lhs, rhs): (Vec<u32>, Vec<u32>) = input
+            .lines()
+            .filter_map(|line| {
+                let mut parts = line.split_whitespace();
+                Some((
+                    parts.next()?.parse::<u32>().ok()?,
+                    parts.next()?.parse::<u32>().ok()?,
+                ))
+            })
+            .unzip();
+
+        let rhs_counts: HashMap<u32, usize> =
+            rhs.iter().copied().fold(HashMap::new(), |mut acc, x| {
+                *acc.entry(x).or_insert(0) += 1;
+                acc
+            });
+
+        let sum: usize = lhs.iter().fold(0, |acc, &l| {
+            acc + rhs_counts.get(&l).copied().unwrap_or(0) * l as usize
+        });
+
+        sum.to_string()
     }
 }
 
@@ -50,5 +71,11 @@ mod tests {
     fn part_one() {
         let day01 = Day01;
         assert_eq!(day01.part_one(TEST_INPUT), "11");
+    }
+
+    #[test]
+    fn part_two() {
+        let day01 = Day01 {};
+        assert_eq!(day01.part_two(TEST_INPUT), "31");
     }
 }
