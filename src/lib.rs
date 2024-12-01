dca37d3a840a84a15137babffce88218403e6812
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-12-01 15:48:19 +0100

    feat: setup project

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::u8;
+
+pub trait Problem {
+    fn part_one(&self, input: &str) -> String;
+    fn part_two(&self, input: &str) -> String;
+
+    fn run(&self, day: u8) {
+        let input = read_input(day);
+        println!("Part one: {}", self.part_one(&input));
+        println!("Part two: {}", self.part_two(&input));
+    }
+}
+
+pub fn read_input(day: u8) -> String {
+    let path = format!("inputs/day{:02}.txt", day);
+    std::fs::read_to_string(&path).unwrap_or_else(|_| panic!("Could not read file {}", &path))
+}
