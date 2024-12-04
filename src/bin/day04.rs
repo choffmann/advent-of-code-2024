8095db092823bf183b01e136f6afc659df3d93b7
Cedrik Hoffmann <choffmann@progeek.de>
2024-12-04 11:14:59 +0100

    feat: solve day04 part 2

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,5 +1,5 @@
 use core::str;
-use std::char;
+use std::{char, collections::HashMap};
 
 use aoc_2024::Problem;
 
@@ -64,7 +64,55 @@ impl Problem for Day04 {
     }
 
     fn part_two(&self, input: &str) -> String {
-        "todo".to_string()
+        let fields: Vec<Vec<char>> = input
+            .trim()
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let directions = [
+            (1, 1),   // diagonal down right
+            (-1, -1), // diagonal up left
+            (1, -1),  // diagonal down left
+            (-1, 1),  // diagonal up right
+        ];
+
+        let mut count_map: HashMap<(usize, usize), ()> = HashMap::new();
+        for (i, row) in fields.iter().enumerate() {
+            for (j, _) in row.iter().enumerate() {
+                if fields[i][j] == 'A' {
+                    let mut word_count = 0;
+                    for (k, &(di, dj)) in directions.iter().enumerate() {
+                        let oi = i as isize - 1 * di;
+                        let oj = j as isize - 1 * dj;
+                        let mut word = String::new();
+                        for l in 0..3 {
+                            let ni = oi + l as isize * directions[k].0;
+                            let nj = oj + l as isize * directions[k].1;
+
+                            if ni < 0
+                                || nj < 0
+                                || ni >= fields.len() as isize
+                                || nj >= fields[0].len() as isize
+                            {
+                                break;
+                            }
+
+                            word.push(fields[ni as usize][nj as usize]);
+                        }
+
+                        if word == "MAS" {
+                            word_count += 1;
+                        }
+                    }
+                    if word_count == 2 {
+                        count_map.insert((i, j), ());
+                    }
+                }
+            }
+        }
+
+        count_map.len().to_string()
     }
 }
 
@@ -97,6 +145,6 @@ MXMXAXMASX
     #[test]
     fn part_two() {
         let day = Day04;
-        assert_eq!(day.part_one(TEST_INPUT), "9");
+        assert_eq!(day.part_two(TEST_INPUT), "9");
     }
 }
